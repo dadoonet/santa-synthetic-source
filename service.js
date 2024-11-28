431ff1d81acda0b5b29177301c3635645785d736
David Pilato <david@pilato.fr>
2024-11-28 17:43:40 +0100

    Activate the service

--- service.js
+++ service.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const messageElement = document.getElementById('message');
     const currentDate = new Date();
-    const openingDate = new Date('2024-11-29T00:00:00');
+    const openingDate = new Date('2024-11-28T00:00:00');
 
     if (currentDate >= openingDate) {
         messageElement.textContent = 'The service is now open!';
